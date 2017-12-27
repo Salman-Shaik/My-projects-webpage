74d86924fbc94d6af6265f575c5b7a04677a68e0
salman shaik <dukesalman5@gmail.com>
2017-12-27 14:38:09 +0530

    added Obstruction Game module

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const tictactoeGame=function(){
 const wordrushGame=function(){
   window.open('https://im-idiot.github.io/WordRush/');
 }
+const obstructionGame=function(){
+  window.open('https://im-idiot.github.io/Obstruction/');
+}
 const addClickListenerToButtons=function() {
   let tictactoe=document.getElementById('tictactoe');
   let wordrush=document.getElementById('wordrush');
+  let obstruction=document.getElementById('obstruction');
+  // let wordrush=document.getElementById('wordrush');
+  // let wordrush=document.getElementById('wordrush');
+  // let wordrush=document.getElementById('wordrush');
+
   wordrush.onclick=wordrushGame;
   tictactoe.onclick=tictactoeGame;
+  obstruction.onclick=obstructionGame;
+  // wordrush.onclick=wordrushGame;
+  // wordrush.onclick=wordrushGame;
+  // wordrush.onclick=wordrushGame;
 }
 const begin=function() {
   addClickListenerToButtons();

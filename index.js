3dc32550dcf62eb4556ce9f4f248a88cbf00a82b
salman shaik <dukesalman5@gmail.com>
2017-12-27 14:48:37 +0530

    added Chomp Game module

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const wordrushGame=function(){
 const obstructionGame=function(){
   window.open('https://im-idiot.github.io/Obstruction/');
 }
+const chompGame=function(){
+  window.open('https://im-idiot.github.io/chomp/');
+}
 const addClickListenerToButtons=function() {
   let tictactoe=document.getElementById('tictactoe');
   let wordrush=document.getElementById('wordrush');
   let obstruction=document.getElementById('obstruction');
-  // let wordrush=document.getElementById('wordrush');
+  let chomp=document.getElementById('chomp');
   // let wordrush=document.getElementById('wordrush');
   // let wordrush=document.getElementById('wordrush');
 
   wordrush.onclick=wordrushGame;
   tictactoe.onclick=tictactoeGame;
   obstruction.onclick=obstructionGame;
-  // wordrush.onclick=wordrushGame;
+  chomp.onclick=chompGame;
   // wordrush.onclick=wordrushGame;
   // wordrush.onclick=wordrushGame;
 }
